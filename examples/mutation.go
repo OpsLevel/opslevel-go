72d2e3a3930c3bc1eba8b42aada7597d8fb6c0b3
Edgar Opslevel <edgaropslevel@edgars-mbp-2.lan>
2022-02-01 16:42:25 -0600

    Move examples to subdir

---
+++ examples/mutation.go
@@ -0,0 +1,19 @@
+var mutation struct {
+	Payload struct {
+		Aliases []graphql.String
+		OwnerId graphql.String
+		Errors  []opslevel.OpsLevelErrors
+	} `graphql:"aliasCreate(input: $input)"`
+}
+variables := PayloadVariables{
+	"input": opslevel.AliasCreateInput{
+		Alias:   "MyNewAlias",
+		OwnerId: "XXXXXXXXXXX",
+	},
+}
+if err := client.Mutate(&mutation, variables); err != nil {
+	panic(err)
+}
+for _, alias := range m.Payload.Aliases {
+	fmt.Println(alias)
+}
---
+++ examples/query.go
@@ -0,0 +1,11 @@
+var query struct {
+	Account struct {
+		Tiers []Tier
+	}
+}
+if err := client.Query(&query, nil); err != nil {
+	panic(err)
+}
+for _, tier := range m.Account.Tiers {
+	fmt.Println(tier.Name)
+}
