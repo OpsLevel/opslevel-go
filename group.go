712caf7845f3cd5bb408f459faed04d6603f1b0b
Edgar Ochoa <edgar@opslevel.com>
2022-04-01 11:31:23 -0500

    Add getgroupwithalias test and function

--- group.go
+++ group.go
@@ -5,7 +5,8 @@ import (
 )
 
 type GroupId struct {
-	Id graphql.ID `json:"id"`
+	Alias string     `json:"alias,omitempty"`
+	Id    graphql.ID `json:"id"`
 }
 
 type GroupParent struct {
@@ -14,12 +15,23 @@ type GroupParent struct {
 }
 
 type Group struct {
+	// DescendantRepositories RepositoryConnection `json:"descendantRepositories,omitempty"`
+	// DescendantServices ServiceConnection `json:"descendantServices,omitempty"`
+	// DescendantSubgroups    SubgroupConnection   `json:"descendantSubgroups,omitempty"`
+	// DescendantTeams        TeamConnection       `json:"descendantTeams,omitempty"`
 	GroupId
-	Alias       string      `json:"alias,omitempty"`
-	Name        string      `json:"name,omitempty"`
-	Description string      `json:"description,omitempty"`
-	Parent      GroupParent `json:"parent,omitempty"`
+	Description string `json:"description,omitempty"`
+	HtmlURL     string `json:"htmlUrl,omitempty"`
+	// Members     UserConnection `json:"members,omitempty"`
+	Name   string      `json:"name,omitempty"`
+	Parent GroupParent `json:"parent,omitempty"`
 }
+
+// type SubgroupConnection struct {
+// 	nodes      []GroupId
+// 	PageInfo   PageInfo
+// 	TotalCount graphql.Int
+// }
 
 func (client *Client) GetGroup(id graphql.ID) (*Group, error) {
 	var q struct {
@@ -29,6 +41,21 @@ func (client *Client) GetGroup(id graphql.ID) (*Group, error) {
 	}
 	v := PayloadVariables{
 		"group": id,
+	}
+	if err := client.Query(&q, v); err != nil {
+		return nil, err
+	}
+	return &q.Account.Group, nil
+}
+
+func (client *Client) GetGroupWithAlias(alias string) (*Group, error) {
+	var q struct {
+		Account struct {
+			Group Group `graphql:"group(alias: $group)"`
+		}
+	}
+	v := PayloadVariables{
+		"group": graphql.String(alias),
 	}
 	if err := client.Query(&q, v); err != nil {
 		return nil, err
--- group_test.go
+++ group_test.go
@@ -18,3 +18,14 @@ func TestGetGroup(t *testing.T) {
 	autopilot.Equals(t, "test_group_1", result.Alias)
 	autopilot.Equals(t, nil, result.Parent.GroupId.Id)
 }
+
+func TestGetGroupWithAlias(t *testing.T) {
+	// Arrange
+	client := ATestClientAlt(t, "group/get", "group/get_with_alias")
+	// Act
+	result, err := client.GetGroupWithAlias("test_group_1")
+	// Assert
+	autopilot.Ok(t, err)
+	autopilot.Equals(t, "test_group_1", result.Alias)
+	autopilot.Equals(t, nil, result.Parent.GroupId.Id)
+}
