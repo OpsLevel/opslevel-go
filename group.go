2cb77ca31a9095830751b9e0e3e9067a514f0470
Kyle Rockman <rocktavious@icloud.com>
2022-04-11 07:38:40 -0500

    Prepare Group Hydration logic for new subresource setup

--- group.go
+++ group.go
@@ -51,6 +51,8 @@ type GroupConnection struct {
 	TotalCount graphql.Int
 }
 
+//#region Create
+
 func (client *Client) CreateGroup(input GroupCreateInput) (*Group, error) {
 	var m struct {
 		Payload struct {
@@ -69,6 +71,110 @@ func (client *Client) CreateGroup(input GroupCreateInput) (*Group, error) {
 	}
 	return &m.Payload.Group, FormatErrors(m.Payload.Errors)
 }
+
+//#endregion
+
+//#region Retrieve
+
+func (client *Client) GetGroup(id graphql.ID) (*Group, error) {
+	var q struct {
+		Account struct {
+			Group Group `graphql:"group(id: $group)"`
+		}
+	}
+	v := PayloadVariables{
+		"group": id,
+	}
+	if err := client.Query(&q, v); err != nil {
+		return nil, err
+	}
+	return &q.Account.Group, nil
+}
+
+func (client *Client) GetGroupWithAlias(alias string) (*Group, error) {
+	var q struct {
+		Account struct {
+			Group Group `graphql:"group(alias: $group)"`
+		}
+	}
+	v := PayloadVariables{
+		"group": graphql.String(alias),
+	}
+	if err := client.Query(&q, v); err != nil {
+		return nil, err
+	}
+	return &q.Account.Group, nil
+}
+
+func (conn *GroupConnection) Query(client *Client, q interface{}, v PayloadVariables) ([]Group, error) {
+	if err := client.Query(q, v); err != nil {
+		return conn.Nodes, err
+	}
+	if err := conn.Hydrate(client); err != nil {
+		return conn.Nodes, err
+	}
+	return conn.Nodes, nil
+}
+
+func (conn *GroupConnection) Hydrate(client *Client) error {
+	var q struct {
+		Account struct {
+			Groups GroupConnection `graphql:"groups(after: $after, first: $first)"`
+		}
+	}
+	v := PayloadVariables{
+		"first": client.pageSize,
+	}
+	q.Account.Groups.PageInfo = conn.PageInfo
+	for q.Account.Groups.PageInfo.HasNextPage {
+		v["after"] = q.Account.Groups.PageInfo.End
+		if err := client.Query(&q, v); err != nil {
+			return err
+		}
+		for _, item := range q.Account.Groups.Nodes {
+			conn.Nodes = append(conn.Nodes, item)
+		}
+	}
+	conn.Nodes = conn.Nodes
+	return nil
+}
+
+func (client *Client) ListGroups() ([]Group, error) {
+	var q struct {
+		Account struct {
+			Groups GroupConnection `graphql:"groups(after: $after, first: $first)"`
+		}
+	}
+	v := client.InitialPageVariables()
+	return q.Account.Groups.Query(client, &q, v)
+}
+
+//#endregion
+
+//#region Update
+
+func (client *Client) UpdateGroup(input GroupUpdateInput) (*Group, error) {
+	var m struct {
+		Payload struct {
+			Group  Group
+			Errors []OpsLevelErrors
+		} `graphql:"groupUpdate(input: $input)"`
+	}
+	v := PayloadVariables{
+		"input": input,
+	}
+	if err := client.Mutate(&m, v); err != nil {
+		return nil, err
+	}
+	if err := m.Payload.Group.Hydrate(client); err != nil {
+		return &m.Payload.Group, err
+	}
+	return &m.Payload.Group, FormatErrors(m.Payload.Errors)
+}
+
+//#endregion
+
+//#region Delete
 
 func (client *Client) DeleteGroupWithAlias(alias string) error {
 	var m struct {
@@ -108,112 +214,4 @@ func (client *Client) DeleteGroup(id graphql.ID) error {
 	return FormatErrors(m.Payload.Errors)
 }
 
-func (s *Group) Hydrate(client *Client) error {
-	// TODO: Will need to hydrate descendants and members here
-	return nil
-}
-
-func (client *Client) GetGroup(id graphql.ID) (*Group, error) {
-	var q struct {
-		Account struct {
-			Group Group `graphql:"group(id: $group)"`
-		}
-	}
-	v := PayloadVariables{
-		"group": id,
-	}
-	if err := client.Query(&q, v); err != nil {
-		return nil, err
-	}
-	if err := q.Account.Group.Hydrate(client); err != nil {
-		return &q.Account.Group, err
-	}
-	return &q.Account.Group, nil
-}
-
-func (client *Client) GetGroupWithAlias(alias string) (*Group, error) {
-	var q struct {
-		Account struct {
-			Group Group `graphql:"group(alias: $group)"`
-		}
-	}
-	v := PayloadVariables{
-		"group": graphql.String(alias),
-	}
-	if err := client.Query(&q, v); err != nil {
-		return nil, err
-	}
-	return &q.Account.Group, nil
-}
-
-func (conn *GroupConnection) Query(client *Client, q interface{}, v PayloadVariables) ([]Group, error) {
-	if err := client.Query(q, v); err != nil {
-		return conn.Nodes, err
-	}
-	if err := conn.Hydrate(client); err != nil {
-		return conn.Nodes, err
-	}
-	return conn.Nodes, nil
-}
-
-func (conn *GroupConnection) Hydrate(client *Client) error {
-	var q struct {
-		Account struct {
-			Groups GroupConnection `graphql:"groups(after: $after, first: $first)"`
-		}
-	}
-	v := PayloadVariables{
-		"first": client.pageSize,
-	}
-	hydratedGroups := GroupConnection{}
-	for _, item := range conn.Nodes {
-		if err := (&item).Hydrate(client); err != nil {
-			return err
-		}
-		hydratedGroups.Nodes = append(hydratedGroups.Nodes, item)
-	}
-	q.Account.Groups.PageInfo = conn.PageInfo
-	for q.Account.Groups.PageInfo.HasNextPage {
-		v["after"] = q.Account.Groups.PageInfo.End
-		if err := client.Query(&q, v); err != nil {
-			return err
-		}
-		for _, item := range q.Account.Groups.Nodes {
-			if err := (&item).Hydrate(client); err != nil {
-				return err
-			}
-			hydratedGroups.Nodes = append(hydratedGroups.Nodes, item)
-		}
-	}
-	conn.Nodes = hydratedGroups.Nodes
-	return nil
-}
-
-func (client *Client) ListGroups() ([]Group, error) {
-	var q struct {
-		Account struct {
-			Groups GroupConnection `graphql:"groups(after: $after, first: $first)"`
-		}
-	}
-	v := client.InitialPageVariables()
-	return q.Account.Groups.Query(client, &q, v)
-}
-
-func (client *Client) UpdateGroup(input GroupUpdateInput) (*Group, error) {
-	var m struct {
-		Payload struct {
-			Group  Group
-			Errors []OpsLevelErrors
-		} `graphql:"groupUpdate(input: $input)"`
-	}
-	v := PayloadVariables{
-		"input": input,
-	}
-	if err := client.Mutate(&m, v); err != nil {
-		return nil, err
-	}
-	if err := m.Payload.Group.Hydrate(client); err != nil {
-		return &m.Payload.Group, err
-	}
-	return &m.Payload.Group, FormatErrors(m.Payload.Errors)
-}
+//#endregion
