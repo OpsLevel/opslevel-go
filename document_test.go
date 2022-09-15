d742cb0fbae65a83f873328030efb8d83f61f29b
Rocktavious <rocktavious@icloud.com>
2022-09-15 08:18:55 -0500

    Fix tests - expand coverage

--- document_test.go
+++ document_test.go
@@ -7,22 +7,34 @@ import (
 	"github.com/rocktavious/autopilot"
 )
 
-//func TestServiceApiDocSettingsUpdate(t *testing.T) {
-//	// Arrange
-//	client := ATestClient(t, "service/api_doc_settings")
-//	// Act
-//	docSource := ol.ApiDocumentSourceEnumPull
-//	result, err := client.ServiceApiDocSettingsUpdate("service_alias", "/src/swagger.json", &docSource)
-//	// Assert
-//	autopilot.Ok(t, err)
-//	autopilot.Equals(t, "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS8zOTI4MQ", result.Id)
-//	autopilot.Equals(t, ol.ApiDocumentSourceEnumPull, *result.PreferredApiDocumentSource)
-//	autopilot.Equals(t, "/src/swagger.json", result.ApiDocumentPath)
-//}
+func TestServiceApiDocSettingsUpdate(t *testing.T) {
+	// Arrange
+	client := ATestClient(t, "service/api_doc_settings")
+	// Act
+	docSource := ol.ApiDocumentSourceEnumPull
+	result, err := client.ServiceApiDocSettingsUpdate("service_alias", "/src/swagger.json", &docSource)
+	// Assert
+	autopilot.Ok(t, err)
+	autopilot.Equals(t, "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS8zOTI4MQ", result.Id)
+	autopilot.Equals(t, ol.ApiDocumentSourceEnumPull, *result.PreferredApiDocumentSource)
+	autopilot.Equals(t, "/src/swagger.json", result.ApiDocumentPath)
+}
 
 func TestServiceApiDocSettingsUpdateDocSourceNull(t *testing.T) {
 	// Arrange
-	client := ATestClient(t, "service/api_doc_settings")
+	client := ATestClientAlt(t, "service/api_doc_settings", "service/api_doc_settings_source_null")
+	// Act
+	result, err := client.ServiceApiDocSettingsUpdate("service_alias", "/src/swagger.json", nil)
+	// Assert
+	autopilot.Ok(t, err)
+	autopilot.Equals(t, "Z2lkOi8vb3BzbGV2ZWwvU2VydmljZS8zOTI4MQ", result.Id)
+	autopilot.Equals(t, ol.ApiDocumentSourceEnumPull, *result.PreferredApiDocumentSource)
+	autopilot.Equals(t, "/src/swagger.json", result.ApiDocumentPath)
+}
+
+func TestServiceApiDocSettingsUpdateDocPathNull(t *testing.T) {
+	// Arrange
+	client := ATestClientAlt(t, "service/api_doc_settings", "service/api_doc_settings_path_null")
 	// Act
 	docSource := ol.ApiDocumentSourceEnumPull
 	result, err := client.ServiceApiDocSettingsUpdate("service_alias", "", &docSource)
