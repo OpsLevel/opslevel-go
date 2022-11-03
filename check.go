6bf314a25360c54ce195c9fe9ad03c49235a24f8
Jackie Rauhut <jackierauhut@opslevel.com>
2022-11-03 16:07:54 -0400

    update tagKey to be optional

--- check.go
+++ check.go
@@ -312,7 +312,7 @@ type CheckServiceOwnershipCreateInput struct {
 
 	RequireContactMethod *bool                             `json:"requireContactMethod,omitempty"`
 	ContactMethod        *ServiceOwnershipCheckContactType `json:"contactMethod,omitempty"`
-	TagKey       *string                                    `json:"tagKey"`
+	TagKey       *string                                    `json:"tagKey,omitempt"`
 	TagPredicate *PredicateInput                           `json:"tagPredicate,omitempty"`
 }
 
@@ -321,7 +321,7 @@ type CheckServiceOwnershipUpdateInput struct {
 
 	RequireContactMethod *bool                             `json:"requireContactMethod,omitempty"`
 	ContactMethod        *ServiceOwnershipCheckContactType `json:"contactMethod,omitempty"`
-	TagKey       *string                                   `json:"tagKey"`
+	TagKey       *string                                   `json:"tagKey,omitempt"`
 	TagPredicate *PredicateUpdateInput                     `json:"tagPredicate,omitempty"`
 }
 
