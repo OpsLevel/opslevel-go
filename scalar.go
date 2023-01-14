7e780bdebbd44974b7c12d94b304e21fcdadee9d
Rocktavious <rocktavious@icloud.com>
2023-01-13 23:41:46 -0600

    fix tests

--- scalar.go
+++ scalar.go
@@ -9,9 +9,6 @@ type ID string
 func (s ID) GetGraphQLType() string { return "ID" }
 
 func (s *ID) MarshalJSON() ([]byte, error) {
-	if s == nil {
-		return []byte("\"\""), nil
-	}
 	if *s == "" {
 		return []byte("null"), nil
 	}
