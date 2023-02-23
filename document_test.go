9b4d42cfe7210a28a96ab3474457251e4e347883
Rocktavious <rocktavious@icloud.com>
2023-02-23 12:04:13 -0600

    fix ID checks

--- document_test.go
+++ document_test.go
@@ -1,15 +1,15 @@
 package opslevel_test
 
 import (
-	ol "github.com/opslevel/opslevel-go/v2023"
-	"testing"
+    ol "github.com/opslevel/opslevel-go/v2023"
+    "testing"
 
-	"github.com/rocktavious/autopilot/v2022"
+    "github.com/rocktavious/autopilot/v2022"
 )
 
 func TestServiceApiDocSettingsUpdate(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
     "query": "mutation ServiceApiDocSettingsUpdate($docPath:String$docSource:ApiDocumentSourceEnum$service:IdentifierInput!){serviceApiDocSettingsUpdate(service: $service, apiDocumentPath: $docPath, preferredApiDocumentSource: $docSource){service{apiDocumentPath,description,framework,htmlUrl,id,aliases,language,lifecycle{alias,description,id,index,name},name,owner{alias,id},preferredApiDocument{id,htmlUrl,source{... on ApiDocIntegration{id,name,type},... on ServiceRepository{baseDirectory,displayName,id,repository{id,defaultAlias},service{id,aliases}}},timestamps{createdAt,updatedAt}},preferredApiDocumentSource,product,repos{edges{node{id,defaultAlias},serviceRepositories{baseDirectory,displayName,id,repository{id,defaultAlias},service{id,aliases}}},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor},totalCount},tags{nodes{id,key,value},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor},totalCount},tier{alias,description,id,index,name},timestamps{createdAt,updatedAt},tools{nodes{category,categoryAlias,displayName,environment,id,url,service{id,aliases}},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor},totalCount}},errors{message,path}}}",
 	"variables":{
 		"docPath":"/src/swagger.json",
@@ -19,26 +19,26 @@ func TestServiceApiDocSettingsUpdate(t *testing.T) {
 		}
     }
 }`
-	response := `{"data": {
+    response := `{"data": {
 	"serviceApiDocSettingsUpdate": {
 		"service": {{ template "service_1" }},
 		"errors": []
 	}
 }}`
-	client := ABetterTestClient(t, "service/api_doc_settings_update", request, response)
-	// Act
-	docSource := ol.ApiDocumentSourceEnumPull
-	result, err := client.ServiceApiDocSettingsUpdate("service_alias", "/src/swagger.json", &docSource)
-	// Assert
-	autopilot.Ok(t, err)
-	autopilot.Equals(t, "MTIzNDU2Nzg5MTIzNDU2Nzg5", string(result.Id))
-	autopilot.Equals(t, ol.ApiDocumentSourceEnumPull, *result.PreferredApiDocumentSource)
-	autopilot.Equals(t, "/src/swagger.json", result.ApiDocumentPath)
+    client := ABetterTestClient(t, "service/api_doc_settings_update", request, response)
+    // Act
+    docSource := ol.ApiDocumentSourceEnumPull
+    result, err := client.ServiceApiDocSettingsUpdate("service_alias", "/src/swagger.json", &docSource)
+    // Assert
+    autopilot.Ok(t, err)
+    autopilot.Equals(t, "Z2lkOi8vMTIzNDU2Nzg5OTg3NjU0MzIx", string(result.Id))
+    autopilot.Equals(t, ol.ApiDocumentSourceEnumPull, *result.PreferredApiDocumentSource)
+    autopilot.Equals(t, "/src/swagger.json", result.ApiDocumentPath)
 }
 
 func TestServiceApiDocSettingsUpdateDocSourceNull(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
     "query": "mutation ServiceApiDocSettingsUpdate($docPath:String$docSource:ApiDocumentSourceEnum$service:IdentifierInput!){serviceApiDocSettingsUpdate(service: $service, apiDocumentPath: $docPath, preferredApiDocumentSource: $docSource){service{apiDocumentPath,description,framework,htmlUrl,id,aliases,language,lifecycle{alias,description,id,index,name},name,owner{alias,id},preferredApiDocument{id,htmlUrl,source{... on ApiDocIntegration{id,name,type},... on ServiceRepository{baseDirectory,displayName,id,repository{id,defaultAlias},service{id,aliases}}},timestamps{createdAt,updatedAt}},preferredApiDocumentSource,product,repos{edges{node{id,defaultAlias},serviceRepositories{baseDirectory,displayName,id,repository{id,defaultAlias},service{id,aliases}}},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor},totalCount},tags{nodes{id,key,value},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor},totalCount},tier{alias,description,id,index,name},timestamps{createdAt,updatedAt},tools{nodes{category,categoryAlias,displayName,environment,id,url,service{id,aliases}},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor},totalCount}},errors{message,path}}}",
 	"variables":{
 		"docPath":"/src/swagger.json",
@@ -48,25 +48,25 @@ func TestServiceApiDocSettingsUpdateDocSourceNull(t *testing.T) {
 		}
     }
 }`
-	response := `{"data": {
+    response := `{"data": {
 	"serviceApiDocSettingsUpdate": {
 		"service": {{ template "service_1" }},
 		"errors": []
 	}
 }}`
-	client := ABetterTestClient(t, "service/api_doc_settings_update_doc_source_null", request, response)
-	// Act
-	result, err := client.ServiceApiDocSettingsUpdate("service_alias", "/src/swagger.json", nil)
-	// Assert
-	autopilot.Ok(t, err)
-	autopilot.Equals(t, "MTIzNDU2Nzg5MTIzNDU2Nzg5", string(result.Id))
-	autopilot.Equals(t, ol.ApiDocumentSourceEnumPull, *result.PreferredApiDocumentSource)
-	autopilot.Equals(t, "/src/swagger.json", result.ApiDocumentPath)
+    client := ABetterTestClient(t, "service/api_doc_settings_update_doc_source_null", request, response)
+    // Act
+    result, err := client.ServiceApiDocSettingsUpdate("service_alias", "/src/swagger.json", nil)
+    // Assert
+    autopilot.Ok(t, err)
+    autopilot.Equals(t, "Z2lkOi8vMTIzNDU2Nzg5OTg3NjU0MzIx", string(result.Id))
+    autopilot.Equals(t, ol.ApiDocumentSourceEnumPull, *result.PreferredApiDocumentSource)
+    autopilot.Equals(t, "/src/swagger.json", result.ApiDocumentPath)
 }
 
 func TestServiceApiDocSettingsUpdateDocPathNull(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
     "query": "mutation ServiceApiDocSettingsUpdate($docPath:String$docSource:ApiDocumentSourceEnum$service:IdentifierInput!){serviceApiDocSettingsUpdate(service: $service, apiDocumentPath: $docPath, preferredApiDocumentSource: $docSource){service{apiDocumentPath,description,framework,htmlUrl,id,aliases,language,lifecycle{alias,description,id,index,name},name,owner{alias,id},preferredApiDocument{id,htmlUrl,source{... on ApiDocIntegration{id,name,type},... on ServiceRepository{baseDirectory,displayName,id,repository{id,defaultAlias},service{id,aliases}}},timestamps{createdAt,updatedAt}},preferredApiDocumentSource,product,repos{edges{node{id,defaultAlias},serviceRepositories{baseDirectory,displayName,id,repository{id,defaultAlias},service{id,aliases}}},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor},totalCount},tags{nodes{id,key,value},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor},totalCount},tier{alias,description,id,index,name},timestamps{createdAt,updatedAt},tools{nodes{category,categoryAlias,displayName,environment,id,url,service{id,aliases}},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor},totalCount}},errors{message,path}}}",
 	"variables":{
 		"docPath":null,
@@ -76,19 +76,19 @@ func TestServiceApiDocSettingsUpdateDocPathNull(t *testing.T) {
 		}
     }
 }`
-	response := `{"data": {
+    response := `{"data": {
 	"serviceApiDocSettingsUpdate": {
 		"service": {{ template "service_1" }},
 		"errors": []
 	}
 }}`
-	client := ABetterTestClient(t, "service/api_doc_settings_update_doc_path_null", request, response)
-	// Act
-	docSource := ol.ApiDocumentSourceEnumPull
-	result, err := client.ServiceApiDocSettingsUpdate("service_alias", "", &docSource)
-	// Assert
-	autopilot.Ok(t, err)
-	autopilot.Equals(t, "MTIzNDU2Nzg5MTIzNDU2Nzg5", string(result.Id))
-	autopilot.Equals(t, ol.ApiDocumentSourceEnumPull, *result.PreferredApiDocumentSource)
-	autopilot.Equals(t, "/src/swagger.json", result.ApiDocumentPath)
+    client := ABetterTestClient(t, "service/api_doc_settings_update_doc_path_null", request, response)
+    // Act
+    docSource := ol.ApiDocumentSourceEnumPull
+    result, err := client.ServiceApiDocSettingsUpdate("service_alias", "", &docSource)
+    // Assert
+    autopilot.Ok(t, err)
+    autopilot.Equals(t, "Z2lkOi8vMTIzNDU2Nzg5OTg3NjU0MzIx", string(result.Id))
+    autopilot.Equals(t, ol.ApiDocumentSourceEnumPull, *result.PreferredApiDocumentSource)
+    autopilot.Equals(t, "/src/swagger.json", result.ApiDocumentPath)
 }
--- level_test.go
+++ level_test.go
@@ -1,15 +1,15 @@
 package opslevel_test
 
 import (
-	"testing"
+    "testing"
 
-	ol "github.com/opslevel/opslevel-go/v2023"
-	"github.com/rocktavious/autopilot/v2022"
+    ol "github.com/opslevel/opslevel-go/v2023"
+    "github.com/rocktavious/autopilot/v2022"
 )
 
 func TestCreateRubricLevels(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
 	   "query": "mutation LevelCreate($input:LevelCreateInput!){levelCreate(input: $input){level{alias,description,id,index,name},errors{message,path}}}",
 		"variables":{
 			"input": {
@@ -19,7 +19,7 @@ func TestCreateRubricLevels(t *testing.T) {
 			}
 	   }
 	}`
-	response := `{"data": {
+    response := `{"data": {
 	"levelCreate": {
 		"level": {
 			"alias": "kyle",
@@ -31,27 +31,27 @@ func TestCreateRubricLevels(t *testing.T) {
 		"errors": []
 	}
 	}}`
-	client := ABetterTestClient(t, "rubric/level_create", request, response)
-	// Act
-	result, _ := client.CreateLevel(ol.LevelCreateInput{
-		Name:        "Kyle",
-		Description: "Created By Kyle",
-		Index:       ol.NewInt(4),
-	})
-	// Assert
-	autopilot.Equals(t, "kyle", result.Alias)
-	autopilot.Equals(t, 4, result.Index)
+    client := ABetterTestClient(t, "rubric/level_create", request, response)
+    // Act
+    result, _ := client.CreateLevel(ol.LevelCreateInput{
+        Name:        "Kyle",
+        Description: "Created By Kyle",
+        Index:       ol.NewInt(4),
+    })
+    // Assert
+    autopilot.Equals(t, "kyle", result.Alias)
+    autopilot.Equals(t, 4, result.Index)
 }
 
 func TestGetRubricLevel(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
 	   "query": "query LevelGet($id:ID!){account{level(id: $id){alias,description,id,index,name}}}",
 		"variables":{
 			"id": "Z2lkOi8vb3BzbGV2ZWwvQ2hlY2tsaXN0LzYyMg"
 	   }
 	}`
-	response := `{"data": {
+    response := `{"data": {
 	"account": {
 		"level": {
 			"alias": "bronze",
@@ -62,47 +62,47 @@ func TestGetRubricLevel(t *testing.T) {
 		}
 	}
 	}}`
-	client := ABetterTestClient(t, "rubric/level_get", request, response)
-	// Act
-	result, err := client.GetLevel("Z2lkOi8vb3BzbGV2ZWwvQ2hlY2tsaXN0LzYyMg")
-	// Assert
-	autopilot.Equals(t, nil, err)
-	autopilot.Equals(t, "Bronze", result.Name)
+    client := ABetterTestClient(t, "rubric/level_get", request, response)
+    // Act
+    result, err := client.GetLevel("Z2lkOi8vb3BzbGV2ZWwvQ2hlY2tsaXN0LzYyMg")
+    // Assert
+    autopilot.Equals(t, nil, err)
+    autopilot.Equals(t, "Bronze", result.Name)
 }
 
 func TestGetMissingRubricLevel(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
 	   "query": "query LevelGet($id:ID!){account{level(id: $id){alias,description,id,index,name}}}",
 		"variables":{
 			"id": "Z2lkOi8vb3BzbGV2ZWwvQ2hlY2tsaXN0LzYyMg"
 	   }
 	}`
-	response := `{"data": {
+    response := `{"data": {
 	"account": {
 		"level": null
 	}
 	}}`
-	client := ABetterTestClient(t, "rubric/level_get_missing", request, response)
-	// Act
-	_, err := client.GetLevel("Z2lkOi8vb3BzbGV2ZWwvQ2hlY2tsaXN0LzYyMg")
-	// Assert
-	autopilot.Assert(t, err != nil, "This test should throw an error.")
+    client := ABetterTestClient(t, "rubric/level_get_missing", request, response)
+    // Act
+    _, err := client.GetLevel("Z2lkOi8vb3BzbGV2ZWwvQ2hlY2tsaXN0LzYyMg")
+    // Assert
+    autopilot.Assert(t, err != nil, "This test should throw an error.")
 }
 
 func TestListRubricLevels(t *testing.T) {
-	// Arrange
-	client := ATestClient(t, "rubric/level/list")
-	// Act
-	result, _ := client.ListLevels()
-	// Assert
-	autopilot.Equals(t, 4, len(result))
-	autopilot.Equals(t, "Bronze", result[1].Name)
+    // Arrange
+    client := ATestClient(t, "rubric/level/list")
+    // Act
+    result, _ := client.ListLevels()
+    // Assert
+    autopilot.Equals(t, 4, len(result))
+    autopilot.Equals(t, "Bronze", result[1].Name)
 }
 
 func TestUpdateRubricLevel(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
 	   "query": "mutation LevelUpdate($input:LevelUpdateInput!){levelUpdate(input: $input){level{alias,description,id,index,name},errors{message,path}}}",
 		"variables":{
 			"input": {
@@ -112,28 +112,28 @@ func TestUpdateRubricLevel(t *testing.T) {
 			}
 	   }
 	}`
-	response := `{"data": {
+    response := `{"data": {
 	"levelUpdate": {
 		"level": {{ template "level_1" }},
 		"errors": []
 	}
 	}}`
-	client := ABetterTestClient(t, "rubric/level_update", request, response)
-	// Act
-	result, _ := client.UpdateLevel(ol.LevelUpdateInput{
-		Id:          "MTIzNDU2Nzg5MTIzNDU2Nzg5",
-		Name:        "Example",
-		Description: ol.NewString("An example description"),
-	})
-	// Assert
-	autopilot.Equals(t, "example", result.Alias)
-	autopilot.Equals(t, "Example", result.Name)
-	autopilot.Equals(t, "An example description", result.Description)
+    client := ABetterTestClient(t, "rubric/level_update", request, response)
+    // Act
+    result, _ := client.UpdateLevel(ol.LevelUpdateInput{
+        Id:          "Z2lkOi8vMTIzNDU2Nzg5OTg3NjU0MzIx",
+        Name:        "Example",
+        Description: ol.NewString("An example description"),
+    })
+    // Assert
+    autopilot.Equals(t, "example", result.Alias)
+    autopilot.Equals(t, "Example", result.Name)
+    autopilot.Equals(t, "An example description", result.Description)
 }
 
 func TestUpdateRubricLevelNoName(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
 	   "query": "mutation LevelUpdate($input:LevelUpdateInput!){levelUpdate(input: $input){level{alias,description,id,index,name},errors{message,path}}}",
 		"variables":{
 			"input": {
@@ -142,27 +142,27 @@ func TestUpdateRubricLevelNoName(t *testing.T) {
 			}
 	   }
 	}`
-	response := `{"data": {
+    response := `{"data": {
 	"levelUpdate": {
 		"level": {{ template "level_1" }},
 		"errors": []
 	}
 	}}`
-	client := ABetterTestClient(t, "rubric/level_update_noname", request, response)
-	// Act
-	result, _ := client.UpdateLevel(ol.LevelUpdateInput{
-		Id:          "MTIzNDU2Nzg5MTIzNDU2Nzg5",
-		Description: ol.NewString("An example description"),
-	})
-	// Assert
-	autopilot.Equals(t, "example", result.Alias)
-	autopilot.Equals(t, "Example", result.Name)
-	autopilot.Equals(t, "An example description", result.Description)
+    client := ABetterTestClient(t, "rubric/level_update_noname", request, response)
+    // Act
+    result, _ := client.UpdateLevel(ol.LevelUpdateInput{
+        Id:          "Z2lkOi8vMTIzNDU2Nzg5OTg3NjU0MzIx",
+        Description: ol.NewString("An example description"),
+    })
+    // Assert
+    autopilot.Equals(t, "example", result.Alias)
+    autopilot.Equals(t, "Example", result.Name)
+    autopilot.Equals(t, "An example description", result.Description)
 }
 
 func TestUpdateRubricLevelEmptyDescription(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
 	   "query": "mutation LevelUpdate($input:LevelUpdateInput!){levelUpdate(input: $input){level{alias,description,id,index,name},errors{message,path}}}",
 		"variables":{
 			"input": {
@@ -172,28 +172,28 @@ func TestUpdateRubricLevelEmptyDescription(t *testing.T) {
 			}
 	   }
 	}`
-	response := `{"data": {
+    response := `{"data": {
 	"levelUpdate": {
 		"level": {{ template "level_1" }},
 		"errors": []
 	}
 	}}`
-	client := ABetterTestClient(t, "rubric/level_update_emptydescription", request, response)
-	// Act
-	result, _ := client.UpdateLevel(ol.LevelUpdateInput{
-		Id:          "MTIzNDU2Nzg5MTIzNDU2Nzg5",
-		Name:        "Example",
-		Description: ol.NewString(""),
-	})
-	// Assert
-	autopilot.Equals(t, "example", result.Alias)
-	autopilot.Equals(t, "Example", result.Name)
-	autopilot.Equals(t, "An example description", result.Description)
+    client := ABetterTestClient(t, "rubric/level_update_emptydescription", request, response)
+    // Act
+    result, _ := client.UpdateLevel(ol.LevelUpdateInput{
+        Id:          "Z2lkOi8vMTIzNDU2Nzg5OTg3NjU0MzIx",
+        Name:        "Example",
+        Description: ol.NewString(""),
+    })
+    // Assert
+    autopilot.Equals(t, "example", result.Alias)
+    autopilot.Equals(t, "Example", result.Name)
+    autopilot.Equals(t, "An example description", result.Description)
 }
 
 func TestUpdateRubricLevelNoDescription(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
 	   "query": "mutation LevelUpdate($input:LevelUpdateInput!){levelUpdate(input: $input){level{alias,description,id,index,name},errors{message,path}}}",
 		"variables":{
 			"input": {
@@ -202,27 +202,27 @@ func TestUpdateRubricLevelNoDescription(t *testing.T) {
 			}
 	   }
 	}`
-	response := `{"data": {
+    response := `{"data": {
 	"levelUpdate": {
 		"level": {{ template "level_1" }},
 		"errors": []
 	}
 	}}`
-	client := ABetterTestClient(t, "rubric/level_update_nodescription", request, response)
-	// Act
-	result, _ := client.UpdateLevel(ol.LevelUpdateInput{
-		Id:   "MTIzNDU2Nzg5MTIzNDU2Nzg5",
-		Name: "Example",
-	})
-	// Assert
-	autopilot.Equals(t, "example", result.Alias)
-	autopilot.Equals(t, "Example", result.Name)
-	autopilot.Equals(t, "An example description", result.Description)
+    client := ABetterTestClient(t, "rubric/level_update_nodescription", request, response)
+    // Act
+    result, _ := client.UpdateLevel(ol.LevelUpdateInput{
+        Id:   "Z2lkOi8vMTIzNDU2Nzg5OTg3NjU0MzIx",
+        Name: "Example",
+    })
+    // Assert
+    autopilot.Equals(t, "example", result.Alias)
+    autopilot.Equals(t, "Example", result.Name)
+    autopilot.Equals(t, "An example description", result.Description)
 }
 
 func TestDeleteRubricLevels(t *testing.T) {
-	// Arrange
-	request := `{
+    // Arrange
+    request := `{
 	   "query": "mutation LevelDelete($input:LevelDeleteInput!){levelDelete(input: $input){deletedLevelId,errors{message,path}}}",
 		"variables":{
 			"input": {
@@ -230,15 +230,15 @@ func TestDeleteRubricLevels(t *testing.T) {
 			}
 	   }
 	}`
-	response := `{"data": {
+    response := `{"data": {
 	"levelDelete": {
 		"deletedLevelId": "Z2lkOi8vb3BzbGV2ZWwvTGV2ZWwvNDgw",
 		"errors": []
 	}
 	}}`
-	client := ABetterTestClient(t, "rubric/level_delete", request, response)
-	// Act
-	err := client.DeleteLevel("Z2lkOi8vb3BzbGV2ZWwvTGV2ZWwvNDgw")
-	// Assert
-	autopilot.Equals(t, nil, err)
+    client := ABetterTestClient(t, "rubric/level_delete", request, response)
+    // Act
+    err := client.DeleteLevel("Z2lkOi8vb3BzbGV2ZWwvTGV2ZWwvNDgw")
+    // Assert
+    autopilot.Equals(t, nil, err)
 }
