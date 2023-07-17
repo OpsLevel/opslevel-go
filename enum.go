759fea5875b08fa71254d813c13de9e2c3f1c0d8
Rocktavious <rocktavious@icloud.com>
2023-07-17 11:27:30 -0500

    Add infra resource to alias owner enum

--- enum.go
+++ enum.go
@@ -44,11 +44,12 @@ var AllAlertSourceTypeEnum = []string{
 type AliasOwnerTypeEnum string
 
 const (
-	AliasOwnerTypeEnumService AliasOwnerTypeEnum = "service" // Aliases that are assigned to services.
-	AliasOwnerTypeEnumTeam    AliasOwnerTypeEnum = "team"    // Aliases that are assigned to teams.
-	AliasOwnerTypeEnumSystem  AliasOwnerTypeEnum = "system"  // Aliases that are assigned to systems.
-	AliasOwnerTypeEnumDomain  AliasOwnerTypeEnum = "domain"  // Aliases that are assigned to domains.
-	AliasOwnerTypeEnumGroup   AliasOwnerTypeEnum = "group"   // Aliases that are assigned to groups.
+	AliasOwnerTypeEnumService                 AliasOwnerTypeEnum = "service" // Aliases that are assigned to services.
+	AliasOwnerTypeEnumTeam                    AliasOwnerTypeEnum = "team"    // Aliases that are assigned to teams.
+	AliasOwnerTypeEnumSystem                  AliasOwnerTypeEnum = "system"  // Aliases that are assigned to systems.
+	AliasOwnerTypeEnumDomain                  AliasOwnerTypeEnum = "domain"  // Aliases that are assigned to domains.
+	AliasOwnerTypeEnumGroup                   AliasOwnerTypeEnum = "group"   // Aliases that are assigned to groups.
+	AliasOwnerTypeEnumInfrastructureResource  AliasOwnerTypeEnum = "infrastructure_resource"   // Aliases that are assigned to infrastructure resources
 )
 
 // All AliasOwnerTypeEnum as []string
@@ -58,6 +59,7 @@ var AllAliasOwnerTypeEnum = []string{
 	string(AliasOwnerTypeEnumSystem),
 	string(AliasOwnerTypeEnumDomain),
 	string(AliasOwnerTypeEnumGroup),
+	string(AliasOwnerTypeEnumInfrastructureResource),
 }
 
 // ApiDocumentSourceEnum represents the source used to determine the preferred API document.
