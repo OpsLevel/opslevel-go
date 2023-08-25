06a0b0eb0a7fd2e5305abc31cc356389d9275be7
David Bloss <david@opslevel.com>
2023-08-24 22:28:36 -0500

    add test data into template files

--- secrets.go
+++ secrets.go
@@ -3,7 +3,7 @@ package opslevel
 type Secret struct {
 	Alias      string     `json:"alias"`
 	ID         ID         `json:"id"`
-	Owner      *Team      `json:"team,omitempty"`
+	Owner      TeamId     `json:"team"`
 	Timestamps Timestamps `json:"timestamps"`
 }
 
@@ -15,7 +15,7 @@ type SecretInput struct {
 type SecretsVaultsSecretConnection struct {
 	Nodes      []Secret
 	PageInfo   PageInfo
-	TotalCount int
+	TotalCount int `graphql:"-"`
 }
 
 func (client *Client) CreateSecret(alias string, input SecretInput) (*Secret, error) {
@@ -37,7 +37,7 @@ func (client *Client) CreateSecret(alias string, input SecretInput) (*Secret, er
 func (client *Client) ListSecretsVaultsSecret(variables *PayloadVariables) (SecretsVaultsSecretConnection, error) {
 	var q struct {
 		Account struct {
-			SecretsVaultsSecret SecretsVaultsSecretConnection `graphql:"secretsVaultsSecrets(after: $after, first: $first)"`
+			SecretsVaultsSecrets SecretsVaultsSecretConnection `graphql:"secretsVaultsSecrets(after: $after, first: $first)"`
 		}
 	}
 	if variables == nil {
@@ -46,21 +46,22 @@ func (client *Client) ListSecretsVaultsSecret(variables *PayloadVariables) (Secr
 	if err := client.Query(&q, *variables, WithName("SecretList")); err != nil {
 		return SecretsVaultsSecretConnection{}, err
 	}
-	for q.Account.SecretsVaultsSecret.PageInfo.HasNextPage {
-		(*variables)["after"] = q.Account.SecretsVaultsSecret.PageInfo.End
+	for q.Account.SecretsVaultsSecrets.PageInfo.HasNextPage {
+		(*variables)["after"] = q.Account.SecretsVaultsSecrets.PageInfo.End
 		resp, err := client.ListSecretsVaultsSecret(variables)
 		if err != nil {
 			return SecretsVaultsSecretConnection{}, err
 		}
-		q.Account.SecretsVaultsSecret.Nodes = append(q.Account.SecretsVaultsSecret.Nodes, resp.Nodes...)
-		q.Account.SecretsVaultsSecret.PageInfo = resp.PageInfo
+		q.Account.SecretsVaultsSecrets.Nodes = append(q.Account.SecretsVaultsSecrets.Nodes, resp.Nodes...)
+		q.Account.SecretsVaultsSecrets.PageInfo = resp.PageInfo
 	}
-	return q.Account.SecretsVaultsSecret, nil
+	q.Account.SecretsVaultsSecrets.TotalCount = len(q.Account.SecretsVaultsSecrets.Nodes)
+	return q.Account.SecretsVaultsSecrets, nil
 }
 
-func (client *Client) UpdateSecret(identifier string, secretInput SecretInput) (*Secret, error) {
+func (client *Client) UpdateSecret(identifier ID, secretInput SecretInput) (*Secret, error) {
 	identifierInput := IdentifierInput{
-		Id: *NewID(identifier),
+		Id: identifier,
 	}
 	var m struct {
 		Payload struct {
@@ -76,25 +77,28 @@ func (client *Client) UpdateSecret(identifier string, secretInput SecretInput) (
 	return &m.Payload.Secret, HandleErrors(err, m.Payload.Errors)
 }
 
-func (client *Client) DeleteSecret(identifier string) error {
+func (client *Client) DeleteSecret(identifier ID) error {
+	identifierInput := IdentifierInput{
+		Id: identifier,
+	}
 	var m struct {
 		Payload struct {
 			Errors []OpsLevelErrors `graphql:"errors"`
 		} `graphql:"secretsVaultsSecretDelete(resource: $input)"`
 	}
-	v := PayloadVariables{"input": *NewIdentifier(identifier)}
+	v := PayloadVariables{"input": identifierInput}
 	err := client.Mutate(&m, v, WithName("SecretsVaultsSecretDelete"))
 	return HandleErrors(err, m.Payload.Errors)
 }
 
-func (client *Client) GetSecret(id ID) (*Secret, error) {
+func (client *Client) GetSecret(identifier string) (*Secret, error) {
 	var q struct {
 		Account struct {
-			Secret Secret `graphql:"secret(id: $secret)"`
+			Secret Secret `graphql:"secretsVaultsSecret(input: $input)"`
 		}
 	}
-	v := PayloadVariables{"secret": id}
-	if err := client.Query(&q, v, WithName("SecretGet")); err != nil {
+	v := PayloadVariables{"input": *NewIdentifier(identifier)}
+	if err := client.Query(&q, v, WithName("SecretsVaultsSecret")); err != nil {
 		return nil, err
 	}
 	return &q.Account.Secret, nil
---
+++ secrets_test.go
@@ -0,0 +1,95 @@
+package opslevel_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/opslevel/opslevel-go/v2023"
+	"github.com/rocktavious/autopilot/v2023"
+)
+
+func TestCreateSecret(t *testing.T) {
+	// Arrange
+	request := `{
+  "query": "mutation SecretsVaultsSecretCreate($alias:String!$input:SecretInput!){secretsVaultsSecretCreate(alias: $alias, input: $input){secret{alias,id,owner{alias,id},timestamps{createdAt,updatedAt}},errors{message,path}}}",
+  "variables": {{ template "secret_create_vars" }}
+  }`
+	response := `{{ template "secret_create_response" }}`
+	id2 := opslevel.NewID("Z2lkOi8vOTg3NjU0MzIxMTIzNDU2Nzg5")
+	client := ABetterTestClient(t, "secrets/create", request, response)
+	fmt.Println(client)
+	// Act
+	secretInput := opslevel.SecretInput{
+		Owner: opslevel.IdentifierInput{Id: *id2},
+		Value: "my-secret",
+	}
+	result, err := client.CreateSecret("alias1", secretInput)
+
+	// Assert
+	autopilot.Ok(t, err)
+	autopilot.Equals(t, *id2, result.Owner.Id)
+}
+
+func TestListSecrets(t *testing.T) {
+	// Arrange
+	requests := []TestRequest{
+		{
+			`{"query": "query SecretList($after:String!$first:Int!){account{secretsVaultsSecrets(after: $after, first: $first){nodes{alias,id,owner{alias,id},timestamps{createdAt,updatedAt}},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor}}}}",
+				{{ template "pagination_initial_query_variables" }}
+      }`,
+			`{{ template "secret_list_response_1" }}`,
+		},
+		{
+			`{"query": "query SecretList($after:String!$first:Int!){account{secretsVaultsSecrets(after: $after, first: $first){nodes{alias,id,owner{alias,id},timestamps{createdAt,updatedAt}},pageInfo{hasNextPage,hasPreviousPage,startCursor,endCursor}}}}",
+				{{ template "pagination_second_query_variables" }}
+		    }`,
+			`{{ template "secret_list_response_2" }}`,
+		},
+	}
+
+	client := APaginatedTestClient(t, "secrets/list", requests...)
+	// Act
+	secretsVaultsSecretConnection, err := client.ListSecretsVaultsSecret(nil)
+	secretNode := secretsVaultsSecretConnection.Nodes
+	// Assert
+	autopilot.Ok(t, err)
+	autopilot.Equals(t, 3, secretsVaultsSecretConnection.TotalCount)
+	autopilot.Equals(t, "example_2", secretNode[1].Alias)
+	autopilot.Equals(t, secretNode[1].Alias, secretNode[1].Owner.Alias)
+}
+
+func TestUpdateSecret(t *testing.T) {
+	// Arrange
+	request := `{
+    "query": "mutation SecretsVaultsSecretUpdate($input:SecretInput!$secret:IdentifierInput!){secretsVaultsSecretUpdate(input: $input, secret: $secret){secret{alias,id,owner{alias,id},timestamps{createdAt,updatedAt}},errors{message,path}}}",
+    "variables": {{ template "secret_update_vars" }}
+  }`
+	response := `{{ template "secret_update_response" }}`
+	client := ABetterTestClient(t, "secrets/update", request, response)
+	// Act
+	id2 := opslevel.NewID("Z2lkOi8vOTg3NjU0MzIxMTIzNDU2Nzg5")
+	secretInput := opslevel.SecretInput{
+		Owner: opslevel.IdentifierInput{Id: *id2},
+		Value: "secret_value_2",
+	}
+	result, err := client.UpdateSecret(*id2, secretInput)
+	// Assert
+	autopilot.Ok(t, err)
+	autopilot.Equals(t, *id2, result.ID)
+	autopilot.Equals(t, *id2, result.Owner.Id)
+}
+
+func TestDeleteSecrets(t *testing.T) {
+	// Arrange
+	request := `{
+    "query": "mutation SecretsVaultsSecretDelete($input:IdentifierInput!){secretsVaultsSecretDelete(resource: $input){errors{message,path}}}",
+    "variables": {{ template "secret_delete_vars" }}
+  }`
+	response := `{{ template "secret_delete_response" }}`
+	id1 := opslevel.NewID("Z2lkOi8vMTIzNDU2Nzg5OTg3NjU0MzIx")
+	client := ABetterTestClient(t, "secrets/delete", request, response)
+	// Act
+	err := client.DeleteSecret(*id1)
+	// Assert
+	autopilot.Equals(t, nil, err)
+}
