0e8a0e90263ddd36cf5074cba8c7f0d142634aaa
Rocktavious <rocktavious@icloud.com>
2023-12-05 09:07:53 -0600

    fix linting

--- json.go
+++ json.go
@@ -2,8 +2,9 @@ package opslevel
 
 import (
 	"encoding/json"
-	"github.com/rs/zerolog/log"
 	"strconv"
+
+	"github.com/rs/zerolog/log"
 )
 
 // JSON is a specialized map[string]string to support proper graphql serialization
