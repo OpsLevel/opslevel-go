9dbd6d1a32bba7d9535b1ae0deb2df0fe64c49bc
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-08 23:42:22 -0500

    move parser to opslevel-go, add validation

---
+++ parser/parser.go
@@ -0,0 +1,247 @@
+package localparser
+
+import (
+	"encoding/json"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"strings"
+)
+
+var (
+	resources  = make(map[string]*Resource)
+	functions  = make(map[string]*Function)
+	verbs      = []string{"Create", "Update", "Get", "Delete", "List", "Assign", "Unassign"}
+	ignoreGlob = []string{}
+)
+
+type Function struct {
+	Name   string   `json:"func"`
+	Input  []string `json:"input"`
+	Output []string `json:"output"`
+}
+
+func (fn *Function) String() string {
+	b, _ := json.Marshal(fn)
+	return string(b)
+}
+
+type Resource struct {
+	Name     string    `json:"resource"`
+	Create   *Function `json:"create,omitempty"`
+	Update   *Function `json:"update,omitempty"`
+	Get      *Function `json:"get,omitempty"`
+	Delete   *Function `json:"delete,omitempty"`
+	List     *Function `json:"list,omitempty"`
+	Assign   *Function `json:"assign,omitempty"`
+	Unassign *Function `json:"unassign,omitempty"`
+}
+
+// for typical objects, 5 is perfect
+func (res *Resource) Score() int {
+	var i int
+	if res.Create != nil {
+		i++
+	}
+	if res.Update != nil {
+		i++
+	}
+	if res.Get != nil {
+		i++
+	}
+	if res.Delete != nil {
+		i++
+	}
+	if res.List != nil {
+		i++
+	}
+	if res.Assign != nil {
+		i++
+	}
+	if res.Unassign != nil {
+		i++
+	}
+	return i
+}
+
+func (res *Resource) String() string {
+	b, _ := json.Marshal(res)
+	return string(b)
+}
+
+func cleanTypeName(s string) string {
+	s = strings.TrimPrefix(s, "*")
+	s = strings.TrimPrefix(s, "[]*")
+	s = strings.TrimPrefix(s, "[]")
+	return s
+}
+
+func isResource(s string) bool {
+	s = cleanTypeName(s)
+	if strings.HasSuffix(s, "Input") || strings.HasSuffix(s, "Connection") || strings.ToUpper(s)[0] != s[0] {
+		return false
+	}
+	for _, ignore := range ignoreGlob {
+		if strings.Contains(s, ignore) {
+			return false
+		}
+	}
+	return true
+}
+
+func parse() error {
+	packages, err := parser.ParseDir(token.NewFileSet(), ".", nil, parser.ParseComments)
+	if err != nil {
+		return err
+	}
+	for _, file := range packages["opslevel"].Files {
+		ast.FileExports(file)
+		ast.Inspect(file, func(n ast.Node) bool {
+			var (
+				funcName    string
+				funcInputs  []string
+				funcOutputs []string
+				resName     string
+			)
+
+			// name must contain verb
+			funcDecl, ok := n.(*ast.FuncDecl)
+			if !ok {
+				return true
+			}
+			funcName = funcDecl.Name.Name
+			hasVerb := false
+			for _, v := range verbs {
+				if strings.Contains(funcName, v) {
+					hasVerb = true
+					break
+				}
+			}
+			if !hasVerb {
+				return true
+			}
+
+			// must be a receiver for client
+			if funcDecl.Recv == nil || len(funcDecl.Recv.List) == 0 {
+				return true
+			}
+			for _, field := range funcDecl.Recv.List {
+				if field == nil {
+					return true
+				}
+				for _, name := range field.Names {
+					if name == nil || types.ExprString(field.Type) != "*Client" {
+						return true
+					}
+				}
+			}
+
+			// parse inputs
+			if funcDecl.Type == nil || funcDecl.Type.Params == nil {
+				return true
+			}
+			for _, field := range funcDecl.Type.Params.List {
+				if field == nil {
+					return true
+				}
+				funcInputs = append(funcInputs, types.ExprString(field.Type))
+			}
+
+			// parse outputs
+			if funcDecl.Type == nil || funcDecl.Type.Results == nil {
+				return true
+			}
+			for _, field := range funcDecl.Type.Results.List {
+				if field == nil {
+					return true
+				}
+				fieldType := types.ExprString(field.Type)
+				funcOutputs = append(funcOutputs, fieldType)
+				resName = cleanTypeName(fieldType)
+				if isResource(resName) {
+					resources[resName] = &Resource{
+						Name: resName,
+					}
+				}
+			}
+
+			// save to state
+			functions[funcName] = &Function{
+				Name:   funcName,
+				Input:  funcInputs,
+				Output: funcOutputs,
+			}
+			return true
+		})
+	}
+	return nil
+}
+
+func matchResourcesToFunctions() {
+	for _, res := range resources {
+		for _, verb := range verbs {
+			funcName := fmt.Sprintf("%s%s", verb, res.Name)
+			if v, ok := functions[funcName]; ok {
+				switch verb {
+				case "Create":
+					res.Create = v
+				case "Update":
+					res.Update = v
+				case "Get":
+					res.Get = v
+				case "Delete":
+					res.Delete = v
+				case "List":
+					res.List = v
+				case "Assign":
+					res.Assign = v
+				case "Unassign":
+					res.Unassign = v
+				default:
+					panic("hello world")
+				}
+				// delete(functions, funcName)
+			}
+		}
+	}
+}
+
+func validatedMatchedFunctions() {
+	for _, res := range resources {
+		if res.Score() == 0 {
+			continue
+		}
+
+		if res.Create != nil {
+			validateCreate(res.Create)
+		}
+		if res.Update != nil {
+			validateUpdate(res.Update)
+		}
+		if res.Get != nil {
+			validateGet(res.Get)
+		}
+		if res.Delete != nil {
+			validateDelete(res.Delete)
+		}
+		if res.List != nil {
+			validateList(res.List)
+		}
+	}
+}
+
+func Run() error {
+	var err error
+	err = parse()
+	if err != nil {
+		return err
+	}
+	matchResourcesToFunctions()
+	validatedMatchedFunctions()
+
+	// TODO: write config files in this repo
+
+	return err
+}
---
+++ parser/validate.go
@@ -0,0 +1,133 @@
+package localparser
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	INPUT_NOT_LEN_ONE         = "input is not len 1"
+	INPUT_NOT_LEN_TWO         = "input is not len 2"
+	INPUT_NOT_LEN_ONE_OR_TWO  = "input is not len 1 or len 2"
+	INPUT_NOT_STRING          = "input is not string"
+	INPUT_NOT_INPUT_TYPE      = "input is not an Input"
+	OUTPUT_NOT_POINTER        = "output is not a pointer"
+	OUTPUT_NOT_ERROR          = "output is not a pointer"
+	OUTPUT_NOT_LEN_ONE        = "output is not len 1"
+	OUTPUT_NOT_LEN_TWO        = "output is not len 2"
+	OUTPUT_NOT_LEN_ONE_OR_TWO = "output is not len 1 or len 2"
+)
+
+// res, err := create(input)
+func validateCreate(fn *Function) {
+	inputLenOneInputType(fn)
+	outputLenTwo(fn)
+}
+
+// res, err := update(string, input)
+// res, err := update(input)
+func validateUpdate(fn *Function) {
+	if len(fn.Input) != 1 && len(fn.Input) != 2 {
+		complain(fn, INPUT_NOT_LEN_ONE_OR_TWO)
+	} else if len(fn.Input) == 1 {
+		inputLenOneInputType(fn)
+	} else {
+		inputLenTwo(fn)
+	}
+	outputLenTwo(fn)
+}
+
+// res, err := get(string)
+func validateGet(fn *Function) {
+	inputLenOneString(fn)
+	outputLenTwo(fn)
+}
+
+// res, err := delete(string)
+// err := delete(string)
+func validateDelete(fn *Function) {
+	inputLenOneString(fn)
+	if len(fn.Output) != 1 && len(fn.Output) != 2 {
+		complain(fn, OUTPUT_NOT_LEN_ONE_OR_TWO)
+	} else if len(fn.Output) == 1 {
+		outputLenOne(fn)
+	} else {
+		outputLenTwo(fn)
+	}
+}
+
+// res, err := list(nil)
+func validateList(fn *Function) {
+	if len(fn.Input) != 1 {
+		complain(fn, INPUT_NOT_LEN_ONE)
+	}
+	outputLenTwo(fn)
+}
+
+func inputLenOneString(fn *Function) {
+	if len(fn.Input) != 1 {
+		complain(fn, INPUT_NOT_LEN_ONE)
+	} else {
+		if fn.Input[0] != "string" {
+			complain(fn, INPUT_NOT_STRING)
+		}
+	}
+}
+
+func inputLenOneInputType(fn *Function) {
+	if len(fn.Input) != 1 {
+		complain(fn, INPUT_NOT_LEN_ONE)
+	} else {
+		if !isInputType(fn.Input[0]) {
+			complain(fn, INPUT_NOT_INPUT_TYPE)
+		}
+	}
+}
+
+func inputLenTwo(fn *Function) {
+	if len(fn.Input) != 2 {
+		complain(fn, INPUT_NOT_LEN_TWO)
+	} else {
+		if fn.Input[0] != "string" {
+			complain(fn, INPUT_NOT_STRING)
+		}
+		if !isInputType(fn.Input[1]) {
+			complain(fn, INPUT_NOT_INPUT_TYPE)
+		}
+	}
+}
+
+func outputLenOne(fn *Function) {
+	if len(fn.Output) != 1 {
+		complain(fn, OUTPUT_NOT_LEN_ONE)
+	} else {
+		if fn.Output[0] != "error" {
+			complain(fn, OUTPUT_NOT_ERROR)
+		}
+	}
+}
+
+func outputLenTwo(fn *Function) {
+	if len(fn.Output) != 2 {
+		complain(fn, OUTPUT_NOT_LEN_TWO)
+	} else {
+		if !isPointer(fn.Output[0]) {
+			complain(fn, OUTPUT_NOT_POINTER)
+		}
+		if fn.Output[1] != "error" {
+			complain(fn, OUTPUT_NOT_ERROR)
+		}
+	}
+}
+
+func complain(fn *Function, msg string) {
+	fmt.Printf("%s\n\t%s\n", fn, msg)
+}
+
+func isPointer(s string) bool {
+	return s[0] == '*'
+}
+
+func isInputType(s string) bool {
+	return !isPointer(s) && strings.HasSuffix(s, "Input")
+}
