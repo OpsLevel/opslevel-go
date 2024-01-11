8234f25816457dc8b14f576dd1be60b429cd2330
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-11 00:21:26 -0500

    gen a single file

--- gen/validate.go
+++ gen/validate.go
@@ -11,6 +11,7 @@ var (
 	INPUT_LEN_1_INPUT_TYPE       = regexp.MustCompile(`^[A-Za-z]*Input$`)
 	INPUT_LEN_1_PAYLOAD_VARS_PTR = regexp.MustCompile(`^\*PayloadVariables$`)
 	INPUT_LEN_2                  = regexp.MustCompile(`^(string|ID), [A-Za-z]*Input$`)
+	INPUT_LEN_2_STRINGS          = regexp.MustCompile(`^(string|ID), (string|ID)$`)
 
 	OUTPUT_LEN_1_ERROR          = regexp.MustCompile(`^error$`)
 	OUTPUT_LEN_2                = regexp.MustCompile(`^\*[A-Z][A-Za-z]*, error$`)
@@ -32,13 +33,19 @@ var (
 	// note: list has a special case where input is empty and the output is an array
 	LIST_INPUTS  = []*regexp.Regexp{INPUT_LEN_1_PAYLOAD_VARS_PTR}
 	LIST_OUTPUTS = []*regexp.Regexp{OUTPUT_LEN_2_CONNECTION_PTR}
+
+	ASSIGN_INPUTS  = []*regexp.Regexp{INPUT_LEN_1_INPUT_TYPE}
+	ASSIGN_OUTPUTS = []*regexp.Regexp{OUTPUT_LEN_2_ARRAY, OUTPUT_LEN_2}
+
+	UNASSIGN_INPUTS  = []*regexp.Regexp{INPUT_LEN_2_STRINGS}
+	UNASSIGN_OUTPUTS = []*regexp.Regexp{OUTPUT_LEN_1_ERROR}
 )
 
-func complain(res *Resource, fn *Function, msg string) {
-	fmt.Printf("%s - %s(%s) (%s)\n", msg, fn.Name, strings.Join(fn.Input, ", "), strings.Join(fn.Output, ", "))
+func complain(fn *Function, msg string) {
+	fmt.Printf("%s - %s\n", msg, fn)
 }
 
-func validateFunction(res *Resource, fn *Function, possibleInputs []*regexp.Regexp, possibleOutputs []*regexp.Regexp) {
+func validateFunction(fn *Function, possibleInputs []*regexp.Regexp, possibleOutputs []*regexp.Regexp) {
 	var (
 		inputAsBytes  = []byte(strings.Join(fn.Input, ", "))
 		outputAsBytes = []byte(strings.Join(fn.Output, ", "))
@@ -60,35 +67,43 @@ func validateFunction(res *Resource, fn *Function, possibleInputs []*regexp.Rege
 	}
 
 	if !inputIsValid {
-		complain(res, fn, "unexpected input types")
+		complain(fn, "unexpected input types")
 	}
 	if !outputIsValid {
-		complain(res, fn, "unexpected return types")
+		complain(fn, "unexpected return types")
 	}
 }
 
-func validateResource(res *Resource) {
-	if res.Create != nil {
-		validateFunction(res, res.Create, CREATE_INPUTS, CREATE_OUTPUTS)
+func validateFunctions() {
+	if len(functions) == 0 {
+		return
 	}
-	if res.Update != nil {
-		validateFunction(res, res.Update, UPDATE_INPUTS, UPDATE_OUTPUTS)
-	}
-	if res.Get != nil {
-		validateFunction(res, res.Get, GET_INPUTS, GET_OUTPUTS)
-	}
-	if res.Delete != nil {
-		validateFunction(res, res.Delete, DELETE_INPUTS, DELETE_OUTPUTS)
-	}
-	if res.List != nil {
-		outputAsBytes := []byte(strings.Join(res.List.Output, ", "))
-		// special case for list - ListTiers() ([]Tiers, empty)
-		if res.List.Input == nil {
-			if !OUTPUT_LEN_2_ARRAY.Match(outputAsBytes) {
-				complain(res, res.List, "unexpected list function with no input")
+	fmt.Printf("---> validating total of %d mapped functions\n", len(functions))
+	for _, fn := range functions {
+		if strings.Contains(fn.Name, "Create") || strings.Contains(fn.Name, "Add") || strings.Contains(fn.Name, "Invite") {
+			validateFunction(fn, CREATE_INPUTS, CREATE_OUTPUTS)
+		} else if strings.Contains(fn.Name, "Update") {
+			validateFunction(fn, UPDATE_INPUTS, UPDATE_OUTPUTS)
+		} else if strings.Contains(fn.Name, "Get") {
+			validateFunction(fn, GET_INPUTS, GET_OUTPUTS)
+		} else if strings.Contains(fn.Name, "Delete") || strings.Contains(fn.Name, "Remove") {
+			validateFunction(fn, DELETE_INPUTS, DELETE_OUTPUTS)
+		} else if strings.Contains(fn.Name, "List") {
+			if fn.Input == nil {
+				if OUTPUT_LEN_2_ARRAY.Match([]byte(strings.Join(fn.Output, ", "))) {
+				} else {
+					complain(fn, "unexpected list function with no input")
+				}
+			} else {
+				validateFunction(fn, LIST_INPUTS, LIST_OUTPUTS)
 			}
+		} else if strings.Contains(fn.Name, "Assign") {
+			validateFunction(fn, ASSIGN_INPUTS, ASSIGN_OUTPUTS)
+		} else if strings.Contains(fn.Name, "Unassign") {
+			validateFunction(fn, UNASSIGN_INPUTS, UNASSIGN_OUTPUTS)
 		} else {
-			validateFunction(res, res.List, LIST_INPUTS, LIST_OUTPUTS)
+			complain(fn, "has no validation rule")
 		}
 	}
+	fmt.Println()
 }
