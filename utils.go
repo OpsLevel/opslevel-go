ef2e19a514686d0696a7ca84cf3912e5ef3ee9ab
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-10 20:23:12 -0500

    cont examples fix

--- utils.go
+++ utils.go
@@ -1,11 +1,8 @@
 package opslevel
 
 import (
-	"encoding/json"
-
 	"github.com/go-playground/validator/v10"
 	"github.com/taimoorgit/moredefaults"
-	"gopkg.in/yaml.v3"
 )
 
 var structValidator = validator.New(validator.WithRequiredStructEnabled())
@@ -16,7 +13,7 @@ func IsResourceValid[T any](opslevelResource T) error {
 }
 
 // Apply resource's `default:""` struct tags
-func SetDefaultsFor[T any](opslevelResource *T, key ...string) {
+func SetDefaultsFor[T any](opslevelResource *T) {
 	validator.New(validator.WithRequiredStructEnabled())
 	if err := moredefaults.Set(opslevelResource); err != nil {
 		panic(err)
@@ -24,7 +21,7 @@ func SetDefaultsFor[T any](opslevelResource *T, key ...string) {
 }
 
 // Apply resource's `example:""` struct tags
-func SetExamplesFor[T any](opslevelResource *T, key ...string) {
+func SetExamplesFor[T any](opslevelResource *T) {
 	validator.New(validator.WithRequiredStructEnabled())
 	if err := moredefaults.Set(opslevelResource, "example"); err != nil {
 		panic(err)
@@ -34,40 +31,6 @@ func SetExamplesFor[T any](opslevelResource *T, key ...string) {
 // Make new OpsLevel resource with defaults set
 func NewExampleOf[T any]() T {
 	var opslevelResource T
-	SetDefaultsFor[T](&opslevelResource, "example")
+	SetExamplesFor[T](&opslevelResource)
 	return opslevelResource
-}
-
-// Get JSON formatted string of an OpsLevel resource - also sets defaults
-func JsonOf[T any](opslevelResource T, key ...string) string {
-	switch len(key) {
-	case 0:
-		SetDefaultsFor[T](&opslevelResource)
-	case 1:
-		SetDefaultsFor[T](&opslevelResource, key[0])
-	default:
-		panic("only one 'key' can be passed")
-	}
-	out, err := json.Marshal(opslevelResource)
-	if err != nil {
-		panic(err)
-	}
-	return string(out)
-}
-
-// Generate yaml formatted string of an OpsLevel resource - also sets defaults
-func YamlOf[T any](opslevelResource T, key ...string) string {
-	switch len(key) {
-	case 0:
-		SetDefaultsFor[T](&opslevelResource)
-	case 1:
-		SetDefaultsFor[T](&opslevelResource, key[0])
-	default:
-		panic("only one 'key' can be passed")
-	}
-	out, err := yaml.Marshal(opslevelResource)
-	if err != nil {
-		panic(err)
-	}
-	return string(out)
 }
