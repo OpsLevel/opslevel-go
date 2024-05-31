e9146bbae4a0882f125cec69daea31cde8991b3b
David Bloss <david@opslevel.com>
2024-05-31 10:31:40 -0700

    update predicate validation (#410)

--- predicate.go
+++ predicate.go
@@ -11,13 +11,17 @@ type Predicate struct {
 }
 
 func (p *Predicate) Validate() error {
+	if !slices.Contains(AllPredicateTypeEnum, string(p.Type)) {
+		return fmt.Errorf("Invalidate Predicate type '%s'. Expected one of '%v'", p.Type, AllPredicateTypeEnum)
+	}
+
 	predicatesWithNoValue := []PredicateTypeEnum{
 		PredicateTypeEnumDoesNotExist,
 		PredicateTypeEnumExists,
 	}
 	if slices.Contains(predicatesWithNoValue, p.Type) && p.Value != "" {
 		return fmt.Errorf("Predicate type '%s' cannot have a value. Given value '%s'", p.Type, p.Value)
-	} else if p.Value == "" {
+	} else if !slices.Contains(predicatesWithNoValue, p.Type) && p.Value == "" {
 		return fmt.Errorf("Predicate type '%s' requires a value", p.Type)
 	}
 	return nil
