b1916fa391e189109f62a2e59d05f084a3009075
Rocktavious <rocktavious@icloud.com>
2024-07-30 10:21:14 -0500

    Fix based on review feedback

--- check_service_property.go
+++ check_service_property.go
@@ -2,7 +2,7 @@ package opslevel
 
 type ServicePropertyCheckFragment struct {
 	Property           ServicePropertyTypeEnum `graphql:"serviceProperty"`        // The property of the service that the check will verify.
-	PropertyDefinition *IdentifierInput        `graphql:"propertyDefinition"`     // The secondary key of the property that the check will verify (e.g. the specific custom property).
+	PropertyDefinition *PropertyDefinition     `graphql:"propertyDefinition"`     // The secondary key of the property that the check will verify (e.g. the specific custom property).
 	Predicate          *Predicate              `graphql:"propertyValuePredicate"` // The condition that should be satisfied by the service property value.
 }
 
